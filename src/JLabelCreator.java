f9668b44a2e9afe6fe33cad816ac69950f3f9bf1
Julian Giudice <juliangiudice@hotmail.com>
2023-03-04 20:51:41 -0300

    Correctly created "From" and "To" Labels

--- src/JLabelCreator.java
+++ src/JLabelCreator.java
@@ -4,7 +4,8 @@ import java.awt.*;
 
 public class JLabelCreator extends JLabel{
     public JLabelCreator(Rectangle dimensions, String text, boolean visibility) {
-        new JLabel(text);
+        new JLabel();
+        setText(text);
         setFont(new Font("SansSerif", Font.PLAIN, 14));
         setForeground(Color.BLACK);
         setBounds(dimensions);
--- src/UnitConverter.java
+++ src/UnitConverter.java
@@ -1,5 +1,6 @@
 // Java Modules:
 import java.awt.*;
+import java.util.Objects;
 import javax.swing.*;
 
 public class UnitConverter extends JFrame{
@@ -10,10 +11,11 @@ public class UnitConverter extends JFrame{
     static final ImageIcon BG_IMAGE = new ImageIcon(Toolkit.getDefaultToolkit().getImage(
             UnitConverter.class.getResource("/assets/background_alura.png")));
 
-    static final Rectangle PANEL_1_POS = new Rectangle(WIDTH/2 - 80, 10, 133, 31);
+    static final Rectangle PANEL_1_POS = new Rectangle(WIDTH/2 - 80, 10, 133, 33);
     static final Rectangle PANEL_2_POS = new Rectangle(WIDTH/2 - 80, 41, 133, 33);
-    static final Rectangle PANEL_3_POS = new Rectangle(WIDTH/2 - 80, 39, 133, 29);
+    static final Rectangle PANEL_3_POS = new Rectangle(WIDTH/2 - 80, 74, 133, 33);
     static final Rectangle LABEL_2_POS = new Rectangle(WIDTH/2 - 118, 41, 133, 33);
+    static final Rectangle LABEL_3_POS = new Rectangle(WIDTH/2 - 118, 74, 133, 33);
     static final String[] CATEGORY_OPTIONS = {"-Select an Option-", "Currency", "Length", "Temperature"};
     static final String[] CURRENCY_OPTIONS = {"-Select an Option-", "Argentine Peso", "Dollar", "Euro", "Pounds", "Yen", "Won"};
     static final String[] LENGTH_OPTIONS = {"-Select an Option-", "", "", ""};
@@ -32,7 +34,6 @@ public class UnitConverter extends JFrame{
         // Create Label and Load Background Image:
         JLabel bgImage = new JLabel(BG_IMAGE);
         add(bgImage);
-
         //bgImage.setLayout(new FlowLayout());
 
         // Panel 1 - Create Category Panel:
@@ -43,54 +44,59 @@ public class UnitConverter extends JFrame{
         JPanelCreator lengthPanel = new JPanelCreator(PANEL_2_POS, LENGTH_OPTIONS, false);
         JPanelCreator temperaturePanel = new JPanelCreator(PANEL_2_POS, TEMPERATURE_OPTIONS, false);
 
-        // Create "From" label and add to the left of panel 2:
-        JLabelCreator from = new JLabelCreator(LABEL_2_POS, "From", true);
+        // Panel 3 - Create Currency, Length and Temperature Panels:
+        JPanelCreator currencyPanel2 = new JPanelCreator(PANEL_2_POS, CURRENCY_OPTIONS, false);
+        JPanelCreator lengthPanel2 = new JPanelCreator(PANEL_2_POS, LENGTH_OPTIONS, false);
+        JPanelCreator temperaturePanel2 = new JPanelCreator(PANEL_2_POS, TEMPERATURE_OPTIONS, false);
+
+        // Label 2 - Create "From" Label:
+        JLabelCreator from = new JLabelCreator(LABEL_2_POS, "From", false);
+
+        // Label 3 - Create "To" Label:
+        JLabelCreator to = new JLabelCreator(LABEL_3_POS, "To", false);
 
         // Add Category Panels to Background Label:
-        bgImage.add(from);
-
-        // Add Category Panels to Background Label:
-        currencyPanel.add(from);
+        bgImage.add(categoryPanel);
         bgImage.add(currencyPanel);
         bgImage.add(temperaturePanel);
         bgImage.add(lengthPanel);
 
+        // Add Labels to Background Label:
+        bgImage.add(from);
+        bgImage.add(to);
 
         //Add ActionListener to Category Options JComboBox:
         categoryPanel.options.addActionListener(e -> {
                     String selectedOption = (String) categoryPanel.options.getSelectedItem();
 
                     // Show/Hide relevant panel based on selected option:
-            switch (selectedOption) {
+            switch (Objects.requireNonNull(selectedOption)) {
                 case "-Select an Option-" -> {
                     currencyPanel.setVisible(false);
                     lengthPanel.setVisible(false);
                     temperaturePanel.setVisible(false);
+                    from.setVisible(false);
                 }
                 case "Currency" -> {
                     currencyPanel.setVisible(true);
                     lengthPanel.setVisible(false);
                     temperaturePanel.setVisible(false);
+                    from.setVisible(true);
                 }
                 case "Length" -> {
                     currencyPanel.setVisible(false);
                     lengthPanel.setVisible(true);
                     temperaturePanel.setVisible(false);
+                    from.setVisible(true);
                 }
                 case "Temperature" -> {
                     currencyPanel.setVisible(false);
                     lengthPanel.setVisible(false);
                     temperaturePanel.setVisible(true);
+                    from.setVisible(true);
                 }
             }
                 });
-
-
-        // Add Background Label to Main Screen:
-        setContentPane(bgImage);
-
-        // Add Category Panel to Main Screen
-        add(categoryPanel, BorderLayout.CENTER);
 
         // Make Main Screen Visible:
         setVisible(true);
